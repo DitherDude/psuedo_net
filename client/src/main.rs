32e7dfd0ab193967885dc7264a6c89a5c542c1aa
DitherDude <01choni01@gmail.com>
2025-04-16 06:48:29 +1000

    added client identification

--- client/src/main.rs
+++ client/src/main.rs
@@ -5,19 +5,22 @@ use magic_crypt::{new_magic_crypt, MagicCryptTrait};
 fn main() {
     let stdin = io::stdin();
     let sessionid = sendrequest("`".to_string()).trim_end_matches('\0').to_string();
-    println!("Got Session ID {}", sessionid);
+    // println!("Got Session ID {}", sessionid);
     for ln in stdin.lock().lines() {
-        let mut line = ln.unwrap().to_string();
+        let line = ln.unwrap().to_string();
         let mc = new_magic_crypt!(&sessionid, 256);
         let encrypted_string = mc.encrypt_str_to_base64(&line);
-        println!("{}", sendrequest(encrypted_string));
+        match sendrequest(encrypted_string).trim_end_matches('\0') {
+            "Disconnected Successfully" => return,
+            x => println!("{}", x.to_string()),
+        }
     }
 }
 
 fn sendrequest(line: String) -> String {
     let mut stream = match TcpStream::connect("127.0.0.1:15496"){
         Ok(stream) => stream,
-        Err(e) => panic!("[ERROR] Failed to connect to server: {}", e),
+        Err(e) => panic!("Failed to connect to server: {}", e),
     };
     stream.write(line.as_bytes()).unwrap();
     let mut buffer = [0; 1024];
--- src/main.rs
+++ src/main.rs
@@ -5,21 +5,19 @@ use std::net::{TcpListener, TcpStream};
 //credit to https://www.youtube.com/watch?v=JiuouCJQzSQ for the tutorial
 
 fn main() {
+    if !std::fs::metadata("sessionid.txt").is_ok() {
+        std::fs::File::create("sessionid.txt").unwrap();
+    }
     //credit to Discord User u\866909828042850354 for port number
     let listener = TcpListener::bind("127.0.0.1:15496")
         .expect("[FATAL] Failed to bind to address. Is port in use?");
     println!("[INFO] Server listening on 127.0.0.1:15496");
-    let mut sessionid = "".to_string();
-    for _ in 0..7 {
-        sessionid =
-            sessionid + format!("{:X}", rand::rng().random_range(0..1E100 as u64)).as_str() + ":";
-    }
-    println!("[INFO] Session ID: {}", sessionid.trim_end_matches(':'));
+    //
+    //println!("[INFO] Session ID: {}", sessionid.trim_end_matches(':'));
     for stream in listener.incoming() {
-        let sessionid = sessionid.trim_end_matches(':').to_string();
         match stream {
             Ok(stream) => {
-                std::thread::spawn(move || handle_connection(stream, sessionid));
+                std::thread::spawn(move || handle_connection(stream));
             }
             Err(e) => {
                 eprintln!("[ERROR] Failed to establish connection with client: {}", e)
@@ -28,27 +26,37 @@ fn main() {
     }
 }
 
-fn handle_connection(mut stream: TcpStream, sessionid: String) {
+fn handle_connection(mut stream: TcpStream) {
+    let clientid = stream.peer_addr().unwrap().ip().to_string();
     let mut buffer = [0; 1024];
     stream
         .read(&mut buffer)
         .expect("[ERROR] Received connection from client, but lost request.");
-    let request = String::from_utf8_lossy(&buffer[..])
-        .trim_end_matches('\0')
-        .to_string();
-    if request == "`".to_string() {
-        stream
-            .write(sessionid.as_bytes())
-            .expect("[ERROR] Connected to client, but failed to send session ID.");
-    } else {
-        println!(
-            "Received request \"{}\" from client!",
-            decrypt(request, sessionid)
-        );
-        let response = "Connected Successfully".as_bytes();
-        stream
-            .write(response)
-            .expect("[ERROR] Connected to client, but failed to send response.");
+    match String::from_utf8_lossy(&buffer[..]).trim_end_matches('\0') {
+        "`" => {
+            stream
+                .write(get_sessionid(clientid).as_bytes())
+                .expect("[ERROR] Connected to client, but failed to send session ID.");
+        }
+        x => {
+            match decrypt(x.to_string(), get_sessionid(clientid.clone())).as_str() {
+                "disconnect" => {
+                    remove_client(clientid.clone());
+                    stream
+                        .write("Disconnected Successfully".as_bytes())
+                        .expect("[ERROR] Connected to client, but failed to send response.");
+                    println!("[INFO] Client {} forogtten.", clientid);
+                }
+                req => {
+                    println!("Received request \"{}\" from client!", req);
+                }
+            }
+
+            let response = "Connected Successfully".as_bytes();
+            stream
+                .write(response)
+                .expect("[ERROR] Connected to client, but failed to send response.");
+        }
     }
 }
 fn decrypt(request: String, sessionid: String) -> String {
@@ -57,4 +65,45 @@ fn decrypt(request: String, sessionid: String) -> String {
         Ok(x) => x,
         Err(e) => e.to_string(),
     };
+}
+
+fn get_sessionid(client: String) -> String {
+    let mut file = std::fs::File::open("sessionid.txt").unwrap();
+    let mut contents = String::new();
+    file.read_to_string(&mut contents).unwrap();
+    for line in contents.lines() {
+        if line.starts_with(&client) {
+            return line
+                .to_string()
+                .trim_start_matches(&client)
+                .trim_start()
+                .to_string();
+        }
+    }
+    let mut sessionid = "".to_string();
+    for _ in 0..7 {
+        sessionid =
+            sessionid + format!("{:X}", rand::rng().random_range(0..1E100 as u64)).as_str() + ":";
+    }
+    let sessionid = sessionid.trim_end_matches(':').to_string();
+    contents = contents + "\n" + &client + " " + &sessionid;
+    file = std::fs::File::create("sessionid.txt").unwrap();
+    file.write_all(contents.as_bytes()).unwrap();
+    println!("[INFO] New client {} connected.", client);
+    // println!("[INFO] Session ID: {}", sessionid);
+    return sessionid;
+}
+
+fn remove_client(client: String) {
+    let mut file = std::fs::File::open("sessionid.txt").unwrap();
+    let mut contents = String::new();
+    file.read_to_string(&mut contents).unwrap();
+    let mut new_contents = "".to_string();
+    for line in contents.lines() {
+        if !line.starts_with(&client) {
+            new_contents = new_contents + line + "\n";
+        }
+    }
+    file = std::fs::File::create("sessionid.txt").unwrap();
+    file.write_all(new_contents.as_bytes()).unwrap();
 }
